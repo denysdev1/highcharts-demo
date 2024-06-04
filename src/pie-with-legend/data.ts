1d5dd461705a2d52e804f9f6d8c42f143c06e772
denysdev1 <denys.kleimenov.dev@gmail.com>
2024-06-04 12:15:32 +0300

    Create pie chart

---
+++ src/pie-with-legend/data.ts
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    name: 'Chrome',
+    y: 8.77,
+  },
+  {
+    name: 'Edge',
+    y: 6.82,
+  },
+  {
+    name: 'Firefox',
+    y: 4.63,
+  },
+  {
+    name: 'Safari',
+    y: 2.44,
+  },
+  {
+    name: 'Internet Explorer',
+    y: 2.02,
+  },
+  {
+    name: 'Other',
+    y: 3.28,
+  },
+];
